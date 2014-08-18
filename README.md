75c3b5019461dc14e90dd623db65e62418b44069
tristan <tristan.gomez@gmail.com>
2014-08-19 03:04:58 +0800

    update readme, remove unused column selcetion

---
+++ README.md
@@ -0,0 +1,3 @@
+This app was made to answer this question: https://www.facebook.com/groups/klxrb/permalink/515838138548150/
+
+Related question: http://stackoverflow.com/questions/8874702/need-data-from-rails-join-table-has-many-through
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ActiveRecord::Base
   has_many :memberships
-  has_many :boards, through: :memberships, :select => 'boards.*, memberships.role_mask as role_mask'
+  has_many :boards, through: :memberships, :select => 'memberships.role_mask as role_mask'
 end
